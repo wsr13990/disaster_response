83ca47ae2011c6bfc31cdc5d8e911c9d838c8f5e
wahyuros <wahyu_s_roso@telkomsel.co.id>
2023-06-11 15:37:37 +0700

    Update process data table name

--- data/process_data.py
+++ data/process_data.py
@@ -49,7 +49,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine(f"sqlite:///{database_filename}")
-    df.to_sql("messages", engine, index=False,if_exists='replace')  
+    df.to_sql("disaster_tweet", engine, index=False,if_exists='replace')  
 
 
 def main():

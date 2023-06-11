0bfc5d7f9a30370247e949e9431141e1fb314669
wahyuros <wahyu_s_roso@telkomsel.co.id>
2023-06-11 14:08:48 +0700

    Update app & model generating script

--- app/run.py
+++ app/run.py
@@ -2,35 +2,72 @@ import json
 import plotly
 import pandas as pd
 
+import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+# from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
+
+from sklearn.base import BaseEstimator, TransformerMixin
+import re
 
 
 app = Flask(__name__)
 
 def tokenize(text):
+    # Remove non alphanumeric char
+    text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
+    
+
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
 
     clean_tokens = []
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
-
+        
+    clean_tokens = [w for w in clean_tokens if not w.lower() in stop_words]
     return clean_tokens
 
+class AfterVerbTagExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self,tags_list):
+        self.tags_list=tags_list
+        self.verb_tags=['VB', 'VBP','VBD','VBG','VBN','VBZ']
+    def after_verb_tags(self,text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            if len(pos_tags) > 0:
+                tags = [tag for _,tag in pos_tags]
+                index = [i for i, j in enumerate(tags) if j in self.verb_tags]
+                if len(index)> 0:
+                    if len(pos_tags) > index[0]+1:
+                        tag_after_verb = pos_tags[index[0]+1]
+                        if tag_after_verb in self.tags_list:
+                            return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.after_verb_tags)
+        return pd.DataFrame(X_tagged)
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('disaster_tweet', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -42,6 +79,18 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+
+    aid_needed = df.iloc[:,3:].columns
+
+    union_list = []
+    for col in aid_needed:
+        df_to_append = df[[col]]
+        df_to_append.columns = ["value"]
+        df_to_append["aid_type"] = col.replace("_", " ")
+        union_list.append(df_to_append)
+    df_union_all= pd.concat(union_list,ignore_index=True)
+    aid_counts = df_union_all.groupby('aid_type').sum()['value']
+    aid_needed = [col.replace("_", " ") for col in aid_needed]
     
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
@@ -61,6 +110,24 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=aid_needed,
+                    y=aid_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Aid Needed',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Aid Needed"
                 }
             }
         }
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,127 @@
 import sys
+# import libraries
+import re
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from sqlalchemy import create_engine
+import itertools
 
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.base import BaseEstimator, TransformerMixin
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+
+import joblib
+
+import warnings
+warnings.filterwarnings('ignore')
+
+class AfterVerbTagExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self,tags_list):
+        self.tags_list=tags_list
+        self.verb_tags=['VB', 'VBP','VBD','VBG','VBN','VBZ']
+    def after_verb_tags(self,text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            if len(pos_tags) > 0:
+                tags = [tag for _,tag in pos_tags]
+                index = [i for i, j in enumerate(tags) if j in self.verb_tags]
+                if len(index)> 0:
+                    if len(pos_tags) > index[0]+1:
+                        tag_after_verb = pos_tags[index[0]+1]
+                        if tag_after_verb in self.tags_list:
+                            return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.after_verb_tags)
+        return pd.DataFrame(X_tagged)
 
 def load_data(database_filepath):
-    pass
+	# load data from database
+	engine = create_engine(f"sqlite:///{database_filepath}")
+	df = pd.read_sql_table(con=engine,table_name="disaster_tweet")
+	X = df['message']
+	Y = df.iloc[:,3:].values
+	category_names = df.iloc[:,3:].columns
+	return X,Y,category_names
 
 
 def tokenize(text):
-    pass
+    # Remove non alphanumeric char
+    text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
+    
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+        
+    clean_tokens = [w for w in clean_tokens if not w.lower() in stop_words]
+    return clean_tokens
 
 
 def build_model():
-    pass
+	verb_tags = ['VB', 'VBP','VBD','VBG','VBN','VBZ']
+	noun_tags = ['NN', 'NNS','NNP','NNPS']
+	adjective_tags = ['JJ','JJR','JJS']
+	pronoun_tags = ['PRP','PRP']
+	adverb_tags = ['RB','RBR','RBS']
+	
+	pipeline = Pipeline([
+		('features', FeatureUnion([
+			# TFIDF Features
+			('text_pipeline', Pipeline([
+				('vect', CountVectorizer(tokenizer=tokenize,ngram_range=(1,2))),
+				('tfidf', TfidfTransformer())
+			])),
+			('first_tag_after_verb', FeatureUnion([
+				('verb_after_verb', AfterVerbTagExtractor(tags_list=verb_tags)),
+				('noun_after_verb', AfterVerbTagExtractor(tags_list=noun_tags)),
+				('adj_after_verb', AfterVerbTagExtractor(tags_list=adjective_tags)),
+				('pronoun_after_verb', AfterVerbTagExtractor(tags_list=pronoun_tags)),
+				('adverb_after_verb', AfterVerbTagExtractor(tags_list=adverb_tags)),
+			])),
+		])),
+		('classifier', MultiOutputClassifier(RandomForestClassifier(n_estimators=20,min_samples_split=2))),
+	])
+	return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+	Y_pred = model.predict(X_test)
+	for i in range(Y_test.shape[1]):
+		print(category_names[i])
+		print(classification_report(Y_test.T[i], Y_pred.T[i]))
 
 
 def save_model(model, model_filepath):
-    pass
+	joblib.dump(model, model_filepath)
 
 
 def main():
